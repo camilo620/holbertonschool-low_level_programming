ede61b6537c675a1569e10f37ddb2807f084ae81
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-06 20:46:38 +0000

    Add point 5 answer

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
