dfd84b9245fd67431c4f1fffad6ca94d9361a1ae
Camilo <camilo.pulgarin11@gmail.com>
2020-02-12 01:09:50 -0500

    Add point 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour = 0;
+	int minute;
+
+	while (hour < 24)
+	{
+		minute = 0;
+
+		while (minute < 60)
+		{
+			_putchar(hour / 10);
+			_putchar(hour % 10);
+			_putchar(':');
+			_putchar(minute / 10);
+			_putchar(minute % 10);
+			_putchar('\n');
+			minute++;
+		}
+		hour++;
+	}
+	_putchar('\n');
+}
