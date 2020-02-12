6a3003e35d307cf87d9a2bc42d02aaf58665cbe9
Camilo <camilo.pulgarin11@gmail.com>
2020-02-12 04:13:03 -0500

    Hot fix at point 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,16 @@
 /**
   * add - adds 2 numbers
   *
-  *Return: result of the addition
+  * Return: result of the addition
+  *
+  * @a: one of the numbers to add
+  *
+  * @b: the second number to add
   *
   */
 int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
