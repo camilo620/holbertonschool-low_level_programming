da34508891ba93ba618d6113f83d95f1119ad81a
Camilo <camilo.pulgarin11@gmail.com>
2020-02-12 02:22:52 -0500

    Add point 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * times_table - prints the table of 9
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int n1 = 0;
+	int n2;
+	int mult;
+
+	while (n1 <= 9)
+	{
+		_putchar('0');
+		n2 = 0;
+
+		while (n2 <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			mult = n1 * n2;
+
+			if (mult <= 10)
+			{
+				_putchar(' ');
+				_putchar(mult + '0');
+			}
+			else
+			{
+				_putchar(mult / 10);
+				_putchar(mult % 10);
+			}
+			n2++;
+		}
+		_putchar('\n');
+		n1++;
+	}
+}
