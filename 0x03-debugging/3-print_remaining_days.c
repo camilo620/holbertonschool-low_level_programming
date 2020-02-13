24d072998ece529073e7fbbe6071c005ac9bcfa2
Camilo <camilo.pulgarin11@gmail.com>
2020-02-13 00:06:06 -0500

    Hot fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,12 +9,11 @@
   * @year: year
   * Return: void
   */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	if (year % 4 == 0)
 	{
-		if (month >= 2 && day >= 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -23,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day >= 60)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
@@ -34,3 +33,4 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
+
