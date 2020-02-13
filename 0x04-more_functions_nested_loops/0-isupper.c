5dbe20f13cb94b86dc1f82beda4c0ac4f06221fc
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-13 17:45:55 +0000

    Hot fix betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
   * _isupper - checks if a character is uppercase
   *
-  * @c - character to be tested
+  * @c: character to be tested
   *
   * Return: 0, 1 depending if they aren't uppercase or actually are
   *
