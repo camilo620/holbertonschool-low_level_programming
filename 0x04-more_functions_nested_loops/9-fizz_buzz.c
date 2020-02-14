1b02546f4ccd7c094d9e9f228f33cb705ea74fda
Camilo <camilo.pulgarin11@gmail.com>
2020-02-13 23:32:55 -0500

    Add point 9 answer

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers and change multiples of 3 and 5 for FizzBuzz
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0 && (n % 3) != 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 5) == 0 && (n % 3) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
