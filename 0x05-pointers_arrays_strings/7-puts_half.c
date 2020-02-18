2e8b3b168825ee0c6e2d1726696c69f69f3e3933
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-18 23:24:02 +0000

    Add point 7 answer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * puts_half - prints last half of the string lenght
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ *
+ */
+
+void puts_half(char *str)
+{
+	int a;
+	int b;
+	int odd;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+	}
+
+	odd = a;
+
+	for (b = (a / 2); b < odd; b++)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -15,6 +15,8 @@ int print_sign(int n);
 
 int _abs(int);
 
+void puts_half(char *str);
+
 int print_last_digit(int);
 
 void jack_bauer(void);
