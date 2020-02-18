ca291419d1a8bfde1023aabc6a428c6cb6b4f382
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-18 22:51:48 +0000

    Add point 6 answer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * puts2 - prints a string but every other character
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ *
+ */
+
+void puts2(char *str)
+{
+	int a;
+	int b;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+	}
+	for (b = 0; b <= a; b++)
+	{
+		if (!(b % 2 != 0))
+		{
+			_putchar(str[b]);
+		}
+	}
+	_putchar('\n');
+}
