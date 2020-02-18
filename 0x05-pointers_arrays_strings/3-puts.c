46421ee81229bb7c9e27d3b477ab0a6221019e43
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-18 20:40:06 +0000

    Add point 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string to stdout
+ *
+ * Return: void
+ *
+ * @str: string to be printed
+ *
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -51,4 +51,6 @@ void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
+void _puts(char *str);
+
 #endif
