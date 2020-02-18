1733ef9533fa6c30114e3c0787065dcd6a19fd21
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-18 20:20:56 +0000

    Add point 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the size of a string 
+ *
+ * Return: size of the string
+ *
+ * @s: string to be sized
+ *
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	return (l);
+}
