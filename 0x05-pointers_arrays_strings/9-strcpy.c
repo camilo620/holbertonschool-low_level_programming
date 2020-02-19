fd75ca86697040ea30a2010c0a286a42818f5fb2
Camilo <camilo.pulgarin11@gmail.com>
2020-02-18 22:30:40 -0500

    Add point 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,3 +11,22 @@
  *
  */
 
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (a = 0; src[a] != 0; a++)
+	{
+	}
+	for (b = 0; b < a; b++)
+	{
+		dest[b] = src[b];
+	}
+	if (b == a)
+	{
+		dest[b] = '\0';
+		b++;
+	}
+	return (dest);
+}
