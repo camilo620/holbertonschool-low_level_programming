46abec0bcab98431bcc80be6345cd3455e5678de
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-20 20:03:33 +0000

    Hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,24 +13,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int a;
-	int b = 0;
 
 		for (a = 0; s1[a] != 0; a++)
 		{
-			if (s1[a] < s2[a])
+			if (s1[a] < s2[a] || s1[a] > s2[a])
 			{
-				b = s1[a] - s2[a];
-				break;
-			}
-			else if (s1[a] > s2[a])
-			{
-				b = s1[a] - s2[a];
-				break;
-			}
-			else
-			{
-				break;
+				return (s1[a] - s2[a]);
 			}
 		}
-	return (b);
+	return (0);
 }
