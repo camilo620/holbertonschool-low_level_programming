11c43ecf6889ea92bc1e23c695e2b086982df392
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-24 19:24:05 +0000

    Add point 4

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -8,7 +8,7 @@
       */
 int main(void)
 {
-	    char *s = "hello";
+	    char *s = "1231231431";
 	        char *f;
 
 		    f = _strchr(s, 'l');
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+unsigned int _strspn(char *s, char *accept);
+
 char *_strchr(char *s, char c);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
