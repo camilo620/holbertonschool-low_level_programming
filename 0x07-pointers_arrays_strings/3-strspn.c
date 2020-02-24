76bcba59e4e497c5b62461cd3ad50ffced8e9f9b
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-24 20:16:36 +0000

    Hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,7 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int b = 0;
 	int c;
 
-	for (a = 0; s[a] != ' '; a++)
+	for (a = 0; s[a] != ' ' && s[a] != 0; a++)
 	{
 		for (c = 0; accept[c] != 0; c++)
 		{
