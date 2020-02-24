e2e1d18ea99a8029163a6cca06342c72efe0e095
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-24 19:52:01 +0000

    Hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,33 +1,32 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+   *
+    *
+     *
+      *
+       *
+        *
+	 *
+	  *
+	   *
+	*
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
 	int a;
-	int b = 0;
+	unsigned int b = 0;
 	int c;
 
 	for (a = 0; s[a] != ' '; a++)
 	{
 		for (c = 0; accept[c] != 0; c++)
 		{
-		if (s[a] == accept[c])
-		{
-			b++;
-			break;
-		}
+			if (s[a] == accept[c])
+			{
+				b++;
+				break;
+			}
 		}
 	}
 	return (b);
