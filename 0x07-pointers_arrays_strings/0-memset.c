716f792f9a3a7d177302f39141168faa7c2c1164
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-24 15:51:28 +0000

    Test of point 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
