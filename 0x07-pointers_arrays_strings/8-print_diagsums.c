da5831a80ff6f742a9d33ee8cefe7934232417a9
Camilo <camilo.pulgarin11@gmail.com>
2020-02-24 23:16:58 -0500

    Add point 8 answer

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,29 @@
 #include "holberton.h"
+#include <stadio.h>
 /**
+ * print_diagsum - prints a sum diagonal
  *
+ * @a: matrix of integers
  *
+ * @size: size of the square
  *
- *
- *
- *
- *
- *
+ * Return: void
  *
  */
 
+void print_diagsums(int *a, int size)
+{
+	int i, incre;
+	int suma1 = 0, suma2 = 0;
 
+	for (i = 0, incre = 0; i < size; i++, incre++)
+	{
+		suma1 += *(a + (size * i) + incre);
+	}
+	for (i = 0, incre = 0; i < size; i++, incre++)
+	{
+		suma2 += *(a + (size * (i + 1)) - (incre + 1));
+	}
+
+	printf("%d, %d\n", suma1, suma2);
+}
