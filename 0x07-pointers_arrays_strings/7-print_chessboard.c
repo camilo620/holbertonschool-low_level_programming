bf7a93d238f913426f5bac234e3737d749793fb2
camilo620 <camilo.pulgarin11@gmail.com>
2020-02-25 00:00:22 +0000

    Add point 7, that actually is 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c;
+	int b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[c][b]);
+		}
+		_putchar('\n');
+	}
+}
