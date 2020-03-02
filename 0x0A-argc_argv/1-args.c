a6f1ed8ce7bab998303ed87a38ed7417b8795163
Camilo <camilo.pulgarin11@gmail.com>
2020-03-01 23:24:12 -0500

    Point 1 answer

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints number of command arguments
+ *
+ * @argc: number of command arguments
+ *
+ * @argv: Array of command arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
