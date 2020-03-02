ddb94b7131bd5299349f583bf6767d85eddef680
Camilo <camilo.pulgarin11@gmail.com>
2020-03-01 23:11:59 -0500

    Hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * main - prints name of the program
  *
+ * @argc: counts the command lines of the program
  *
+ * @argv: array of command lines of the program
  *
- *
+ * Return: 0
  *
  */
 
-void main(char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return (void);
+	return (0);
 }
