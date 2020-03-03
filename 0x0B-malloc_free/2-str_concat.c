59bf80895bcdaa2af8de6d54da8af010c71f0898
Camilo620 <camilo.pulgarin11@gmail.com>
2020-03-03 20:34:29 +0000

    Add point 2 answer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,11 @@ char *str_concat(char *s1, char *s2)
 	int contador2;
 	char *a;
 
+	if (s1 == 0 || s2 == 0)
+	{
+		return (NULL);
+	}
+
 	for (contador = 0; s1[contador] != 0; contador++)
 	{
 	}
