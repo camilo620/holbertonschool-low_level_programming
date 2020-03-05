0bf78a2adf0a3b927ec3158ea05c5976627e05db
Camilo620 <camilo.pulgarin11@gmail.com>
2020-03-05 22:20:51 +0000

    Add point 100 answer

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _realloc - change size of an allocated memory
+ *
+ * @ptr: pointer to memory to be changed
+ *
+ * @old_size: old size of memory
+ *
+ * @new_size: new size of memory
+ *
+ * Return: pointer to new allocated memory
+ *
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int counter;
+	int *holder;
+	int *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		free(ptr);
+		return (new_ptr);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size * sizeof(unsigned int));
+	holder = ptr;
+	if (new_size < old_size)
+	{
+		for (counter = 0; counter <= new_size; counter++)
+			new_ptr[counter] = holder[counter];
+	}
+	if (new_size > old_size)
+	{
+		for (counter = 0; counter <= old_size; counter++)
+			new_ptr[counter] = holder[counter];
+	}
+	free(holder);
+	return (new_ptr);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 int *array_range(int min, int max);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
