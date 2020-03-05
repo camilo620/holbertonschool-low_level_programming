ebe3cc3ce5aa3de448808855729c85d5d485e89f
Camilo620 <camilo.pulgarin11@gmail.com>
2020-03-05 16:19:46 +0000

    Add point 2 answer

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings at a new allocated memory
+ *
+ * @s1: string one to be concatenated
+ *
+ * @s2: string two to be concatenated
+ *
+ * @n: maximum number of characters of s2 to be concatenated
+ *
+ * Return: pointer to allocated memory
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	int contador;
+	unsigned int contador2;
+	char *a;
+
+
+	if (s1 == 0)
+	{
+		s1 = "";
+	}
+	if (s2 == 0)
+	{
+		s2 = "";
+	}
+	for (contador = 0; s1[contador] != 0; contador++)
+	{
+	}
+	for (contador2 = 0; s2[contador2] != 0; contador2++)
+	{
+	}
+	if (contador2 >= n)
+	{
+		contador2 = n;
+	}
+	a = malloc((contador + contador2 + 1) * sizeof(char));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (contador = 0; s1[contador] != 0; contador++)
+	{
+		a[contador] = s1[contador];
+	}
+	for (contador2 = 0; contador2 <= n && s2[contador2] != 0; contador2++)
+	{
+		a[contador] = s2[contador2];
+		contador++;
+	}
+	a[contador] = '\0';
+
+	return (a);
+	
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 void *malloc_checked(unsigned int b);
 
 char *argstostr(int ac, char **av);
