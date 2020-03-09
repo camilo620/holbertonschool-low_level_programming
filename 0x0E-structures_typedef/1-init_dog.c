1bc012405d51ae0860853384d9d199dfd6681962
Camilo620 <camilo.pulgarin11@gmail.com>
2020-03-09 22:23:29 +0000

    add point 2 Answer

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialice an structure
+ *
+ * @d: pointer to structure to initialice
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog *p;
+
+p = d;
+
+p->name = name;
+p->age = age;
+p->owner = owner;
+}
+
