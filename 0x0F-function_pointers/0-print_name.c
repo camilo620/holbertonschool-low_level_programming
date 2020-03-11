7144ff9488813f27b8d0c0ed0aa2d2793632e3f3
Camilo620 <camilo.pulgarin11@gmail.com>
2020-03-11 17:46:16 +0000

    Add point 1 answer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
+ * print_name - prints name with a funtion pointer
  *
+ * @name: name to be printed
  *
- *
- *
- *
- *
- *
- *
- *
- *
+ * @f: funtion pointer to print names
  */
 
 void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTER
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
