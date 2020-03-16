5750223991468203a9a05a411e0adf546dad98f2
Camilo <camilo.pulgarin11@gmail.com>
2020-03-15 22:22:02 -0500

    Hot fix betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,18 +11,18 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list nums;
+	unsigned int i;
+	va_list nums;
 
-    va_start(nums, n);
+	va_start(nums, n);
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(nums, int));
-        if (i < n - 1 && separator != NULL)
-        {
-            printf("%s", separator);
-        }
-    }
-    printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
 }
