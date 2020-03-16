80278dee552fdd2e79116c62f98ca0f381b6b6df
Camilo <camilo.pulgarin11@gmail.com>
2020-03-16 00:24:23 -0500

    Add point 4 answer

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * cha - Prints a character
+ * @arg: arguments
+ * Return: void
+ */
+void cha(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+ * num - Prints integers
+ * @arg: arguments
+ * Return: void
+ */
+void num(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+/**
+ * fl - Prints floats
+ * @arg: arguments
+ * Return: void
+ */
+void fl(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+ * string - prints a string
+ * @arg: arguments
+ * Return: void
+ */
+void string(va_list arg)
+{
+	char *pointer;
+
+	pointer = va_arg(arg, char *);
+	if (pointer == 0)
+	{
+		s = "(nil)";
+	}
+	printf("%s", pointer);
+
+}
+/**
+ * print_all - prints arguments sent
+ * @format: Holds a string that contains identifiers of data types
+ * @...: arguments
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list checker;
+	fun fun2[] = {
+		{"c", cha},
+		{"i", num},
+		{"f", fl},
+		{"s", string},
+		{0, 0}
+	};
+	char *p1 = "", *p2 = ", ";
+
+	i = 0;
+	va_start(checker, format);
+	while (format != 0 && format[i] != 0)
+	{
+		j = 0;
+		while (fun2[j].op != 0)
+		{
+			if (fun2[j].op[0] == format[i])
+			{
+				printf("%s", p1);
+				fun2[j].f(checker);
+				p1 = p2;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(checker);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,17 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct function - Struct type of functions
+ *
+ * @op: The type of function
+ * @f: The function
+ */
+typedef struct function
+{
+	char *op;
+	void (*f)();
+} fun;
 
 #endif
