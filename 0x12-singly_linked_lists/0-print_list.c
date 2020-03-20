7822fabdf07da24e5ec9ae5b9e9e0ffc367e46a8
Camilo <camilo.pulgarin11@gmail.com>
2020-03-19 22:37:37 -0500

    Add point 1 answer

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ */
+size_t print_list(const list_t *h)
+{
+    size_t i = 0;
+
+    if (h->str == NULL)
+    {
+        printf("[0] (nil)\n");
+        h = h->next;
+        i++;
+    }
+    for (; h != NULL; i++)
+    {
+        printf("[%d] %s\n", h->len, h->str);
+        h = h->next;
+    }
+    return (i);
+}
