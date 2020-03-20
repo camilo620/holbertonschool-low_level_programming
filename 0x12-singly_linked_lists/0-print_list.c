1cd103977869da811007a0e508b33ffe21375915
Camilo <camilo.pulgarin11@gmail.com>
2020-03-20 00:38:03 -0500

    Add point 0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    if (h->str == NULL)
-    {
-        printf("[0] (nil)\n");
-        h = h->next;
-        i++;
-    }
-    for (; h != NULL; i++)
-    {
-        printf("[%d] %s\n", h->len, h->str);
-        h = h->next;
-    }
-    return (i);
+	for (; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (i);
 }
