3bde71fbfa019f9d6a4fc9905c48fab2af3a0cf5
Camilo <camilo.pulgarin11@gmail.com>
2020-03-24 17:33:59 -0500

    Add point 6 answer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * free_listint - free allocated memory of the nodes
+ * @head: head of the list
+ * Return (void)
+ */
+
+void free_listint2(listint_t **head)
+{
+	int i;
+	listint_t *temp, *temp2;
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    temp = *head;
+	for (i = 0; temp != 0; i++)
+	{
+		temp2 = temp;
+		temp = temp->next;
+		free(temp2);
+	}
+    *head == 0;
+}
