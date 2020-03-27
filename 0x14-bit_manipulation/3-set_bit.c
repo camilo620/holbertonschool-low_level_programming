f5c0d0e680375337bbb72a2540d949960bb5f4f4
Camilo <camilo.pulgarin11@gmail.com>
2020-03-26 21:21:02 -0500

    Add point 4 answer

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * set_bit - change a bit at index
+ * @n: number to be changed
+ * @index: position to be changed
+ * Return: 1 if success -1 if not
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int i = 1;
+
+	if (index > 64)
+    {
+		return (-1);
+    }
+    
+	i = i << index;
+
+	*n = (*n | i);
+
+	return (1);
+}
