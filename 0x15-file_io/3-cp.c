47cbf6c58ee93a1de0202ada4fc8add00d88b6da
Camilo <camilo.pulgarin11@gmail.com>
2020-03-31 23:45:26 -0500

    Add point 4 answer hotfix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,12 +17,20 @@ int main(int argc, char *argv[])
     {
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
     }
-
 	f = open(argv[1], O_RDONLY);
+    
 	if (f == -1)
+    {
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 		argv[1]), exit(98);
-	
+    }
+	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (f2 == -1)
+    {
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+    }
+
 	for (i = 1024; i == 1024;)
 	{
 		i = read(f, BUFFER, 1024);
@@ -35,10 +43,18 @@ int main(int argc, char *argv[])
 			argv[2]), exit(99);
 	}
 	o = close(f);
+    
 	if (o == -1)
+    {
 		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", f), exit(100);
+    }
+
 	o = close(f2);
+
 	if (o == -1)
+    {
 		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", f2), exit(100);
+    }
+
 return (0);
 }
