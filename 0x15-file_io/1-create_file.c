f20526735e7ba87b6f1f3ab5085ffc23f2c93f0a
Camilo <camilo.pulgarin11@gmail.com>
2020-03-31 23:04:44 -0500

    Hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,8 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i] != 0; i++)
 			;
 		text = write(f, text_content, i);
+		if (text == -1)
+			return (-1);
 		close(f);
 		return (1);
 	}
