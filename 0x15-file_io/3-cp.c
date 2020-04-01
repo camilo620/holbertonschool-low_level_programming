abc03278a8f1ba8be9bcdec04767b9e39b48ae4f
Camilo <camilo.pulgarin11@gmail.com>
2020-03-31 23:42:31 -0500

    Add point 4 answer

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * main - Main
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0 if success, other numbers if different failures occur
+ */
+int main(int argc, char *argv[])
+{
+	int f; 
+    int f2;
+    char BUFFER[1024];
+    int i;
+    int o;
+
+	if (argc != 3)
+    {
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+    }
+
+	f = open(argv[1], O_RDONLY);
+	if (f == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		argv[1]), exit(98);
+	
+	for (i = 1024; i == 1024;)
+	{
+		i = read(f, BUFFER, 1024);
+		if (i == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
+		o = write(f2, BUFFER, i);
+		if (o == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+			argv[2]), exit(99);
+	}
+	o = close(f);
+	if (o == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", f), exit(100);
+	o = close(f2);
+	if (o == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", f2), exit(100);
+return (0);
+}
