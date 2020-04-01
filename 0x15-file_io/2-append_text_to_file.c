79ecbe5f01a4deb47e54fce74287a61e2a620d9a
Camilo <camilo.pulgarin11@gmail.com>
2020-03-31 23:16:02 -0500

    Add point 3 answer

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,17 +11,29 @@ int append_text_to_file(const char *filename, char *text_content)
     int text;
     int i;
 
-	if (filename)
+	if (filename != 0)
 	{
-		if (!text_content)
+		if (text_content == 0)
+        {
 			return (1);
+        }
+            
 		f = open(filename, O_RDWR | O_APPEND);
+
 		if (f == -1)
+        {
 			return (-1);
+        }
+
 		for (i = 0; text_content[i]; i++)
-			;
+        {
+        }
+
 		text = write(f, text_content, i);
-		
+		if (text == -1)
+        {
+			return (-1);
+        }
 		close(f);
 		return (1);
 	}
