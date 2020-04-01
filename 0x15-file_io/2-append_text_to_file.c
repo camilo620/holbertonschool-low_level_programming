7d582bee4954b7454ceabb2569629080d26a1093
Camilo <camilo.pulgarin11@gmail.com>
2020-03-31 23:16:40 -0500

    Hot fix betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,32 +8,32 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int f;
-    int text;
-    int i;
+	int text;
+	int i;
 
 	if (filename != 0)
 	{
 		if (text_content == 0)
-        {
+		{
 			return (1);
-        }
-            
+		}
+			
 		f = open(filename, O_RDWR | O_APPEND);
 
 		if (f == -1)
-        {
+		{
 			return (-1);
-        }
+		}
 
 		for (i = 0; text_content[i]; i++)
-        {
-        }
+		{
+		}
 
 		text = write(f, text_content, i);
 		if (text == -1)
-        {
+		{
 			return (-1);
-        }
+		}
 		close(f);
 		return (1);
 	}
