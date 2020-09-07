6d27400a6078570acdfc8acf3e080178f72151cb
Camilo <camilo.pulgarin11@gmail.com>
2020-09-07 00:12:50 -0500

    Hotfix 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,60 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - insert a node in the idx position.
- *
- * @h: head of the linked list.
- * @idx: position of the node to be inserted.
- * @n: number inside the new node.
- *
- * Return: new node pointer or NULL if it cannot be added.
+ * listlen - size of a list
+ * @head: head of the list
+ * Return: size of the list
  */
+size_t listlen(const dlistint_t *head)
+{
+	size_t len;
 
+	for (len = 0; head; head = head->next, len++)
+		;
+	return (len);
+}
+/**
+ * insert_dnodeint_at_index - Inserts node at idx position.
+ *
+ * @h: head of the list
+ * @idx: index to the new node position.
+ * @n: integer inside the new node
+ *
+ * Return: new node pointer
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	size_t len = listlen(*h);
+	size_t count;
 	dlistint_t *temp = *h;
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-	unsigned int count = 0;
+	dlistint_t *node;
 
-	if (node == NULL)
-		return (NULL);
+	if (len < idx)
+		return (0);
 
-	if (*h)
-	{
-		for (count = 0; temp; temp = temp->next, count++)
-		{
-		}
-		if (idx > count)
-		{
-			return (NULL);
-		}
-		temp = *h;
-		for (count = 0; count <= idx; temp = temp->next, count++)
-		{
-			if (count == idx && temp == NULL)
-			{
-				return (add_dnodeint_end(h, n));
-			}
-			else if (count + 1 == idx)
-			{
-				node->n = n;
-				node->next = temp->next;
-				node->prev = temp;
-				temp->next = node;
-				return (node);
-			}
-		}
-	}
-	if (*h == NULL && idx == 0)
-	{
+	else if (idx == 0)
 		return (add_dnodeint(h, n));
+
+	else if (idx == len)
+		return (add_dnodeint_end(h, n));
+
+	else if (idx != 0 && idx != len)
+	{
+		node = malloc(sizeof(dlistint_t));
+		node->n = n;
+
+		if (node == NULL)
+			return (NULL);
+			for (count = 0; temp; temp = temp->next, count++)
+			{
+				if (count + 1 == idx)
+				{
+					node->next = temp->next;
+					node->prev = temp;
+					temp->next->prev = node;
+					temp->next = node;
+					return (node);
+				}
+			}
 	}
 	return (NULL);
 }
